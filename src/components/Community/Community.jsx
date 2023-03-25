dcc9d698e3a5e01265fba5d5678126125247e9d0
nikhilravi56 <116006185+nikhilravi56@users.noreply.github.com>
2023-03-25 21:23:12 +0530

    Update Community.jsx

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -3,35 +3,19 @@ import YourGame from '../YourGame/YourGame';
 import images from '../../constants/images';
 import MyModal from './ShowModal';
 import './Community.css'
-// import images from "../../constants/images.js";
-
-
 const Community = () => {
   const [showModal,setShowModal]=useState(false);
-
   const closeModal=()=>setShowModal(false);
   return (
-    <div className='Games' id='COMMUNITY'>
-        {/* <button type="button" className="Gamebutton1">
-     YOUR GAMES
-   </button> */}
-    
+    <div className='Games' id='COMMUNITY'>  
     <div className='yourGames' id='community'>
     <YourGame title='Valorant' imagee={images.valo3}  />
-   
       <YourGame title='CSGO' imagee={images.csgo3} />
-      
       <YourGame title='Fall Guys' imagee={images.fall3} />
-     
       <YourGame title='GTA V' imagee={images.gta3} />
-     
       <YourGame title='Forza Horizon' imagee={images.forza3} />
-
       <YourGame title='Witcher 3' imagee={images.witch3}/>
-
       </div>
-
-
     </div>
   )
 }
